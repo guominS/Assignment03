8378b8855c22daf299598a12b32e3db1762f1600
guominS <52800170+guominS@users.noreply.github.com>
2019-07-21 22:06:58 -0400

    Update mySongs.ts

--- assets/data/mySongs.ts
+++ assets/data/mySongs.ts
@@ -1,7 +1,7 @@
 import{song} from '../../app/songInfo';
 
 export const MYSONGS: song[] = [
-    {songName:'FutureBase',artist:'KizunaAI',genre:'pop music',yearReleased:'2018',picturePath:'../../assets/images/yourKizunaAi.jpg',},
+    {songName:'FutureBase',artist:'KizunaAI',genre:'pop music',yearReleased:'2018',picturePath:'../assets/images/yourKizunaAi.jpg',},
     {songName:'symphony 9',artist:'Beethoven',genre:'classic music',yearReleased:'1824',picturePath:'../../assets/images/myBeethoven.jpg',},
     {songName:'What does the fox say?',artist:'Ylvis',genre:'fox music',yearReleased:'2013',picturePath:'../../assets/images/whatDoesFoxSay.jpg',},
     {songName:'the call of cthulhu',artist:'Cthulhu',genre:'San Music',yearReleased:'1925',picturePath:'../../assets/images/mylittleDream.jpg',}
